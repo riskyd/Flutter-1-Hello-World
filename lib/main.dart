9fba7965f6fe84cf946cc1675f2486499e186da5
riskyd <riskydil86@gmail.com>
2022-02-24 19:22:51 +0700

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+void main(){
+  runApp(
+    MaterialApp(
+      home: Material(
+        color: Colors.indigo,
+        child: Center(child: Text('Hello World'),),
+      )
+    ),
+  );
+}
